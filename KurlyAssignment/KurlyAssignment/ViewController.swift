b2635e069f15f7a750391ed7b166d7941fffe26b
krgoodnews <krgoodnews@gmail.com>
2025-12-18 20:56:12 +0900

    create: New Xcode Project

---
+++ KurlyAssignment/KurlyAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KurlyAssignment
+//
+//  Created by Goodnews on 12/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
